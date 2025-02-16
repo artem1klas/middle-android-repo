a60c69f1047f61c2bcd50b4d22b980260d712975
artem1klas <baryshev964086@yandex.ru>
2025-02-17 00:25:52 +0300

    sprint1 attempt1

--- app/src/main/java/com/example/androidpracticumcustomview/MainActivity.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.androidpracticumcustomview
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.ui.graphics.Color
 import com.example.androidpracticumcustomview.ui.theme.CustomContainer
+import com.example.androidpracticumcustomview.ui.theme.MainScreen
 
 /*
 Задание:
@@ -18,28 +22,41 @@ class MainActivity : ComponentActivity() {
         /*
         Раскомментируйте нужный вариант
          */
-        startXmlPracticum() // «традиционный» android (XML)
-//          setContent { // Jetpack Compose
-//             MainScreen()
-    }
-
-    private fun startXmlPracticum() {
-        val customContainer = CustomContainer(this)
-        setContentView(customContainer)
-
-        val firstView = TextView(this).apply {
-            // TODO
-            // ...
+//        startXmlPracticum() // «традиционный» android (XML)
+        setContent { // Jetpack Compose
+            MainScreen()
         }
 
-        val secondView = TextView(this).apply {
-            // TODO
-            // ...
-        }
-
-        // Добавление второго элемента через некоторое время
-        Handler(Looper.getMainLooper()).postDelayed({
-            customContainer.addView(secondView)
-        }, 2000)
+//        private fun startXmlPracticum() {
+//            val customContainer = CustomContainer(this)
+//
+//
+//            val firstView = TextView(this).apply {
+//                text = "UP"
+//                textSize = 24f
+//                layoutParams = ViewGroup.LayoutParams(
+//                    ViewGroup.LayoutParams.WRAP_CONTENT,
+//                    ViewGroup.LayoutParams.WRAP_CONTENT
+//                )
+//
+//            }
+//
+//            val secondView = TextView(this).apply {
+//                text = "DOWN"
+//                textSize = 24f
+//                layoutParams = ViewGroup.LayoutParams(
+//                    ViewGroup.LayoutParams.WRAP_CONTENT,
+//                    ViewGroup.LayoutParams.WRAP_CONTENT
+//                )
+//            }
+//            customContainer.addView(firstView)
+//            customContainer.addView(secondView)
+//            setContentView(customContainer)
+//
+//            // Добавление второго элемента через некоторое время
+////        Handler(Looper.getMainLooper()).postDelayed({
+////            customContainer.addView(secondView)
+////        }, 2000)
+//        }
     }
 }
--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
@@ -1,9 +1,14 @@
 package com.example.androidpracticumcustomview.ui.theme
 
 import android.content.Context
+import android.provider.SyncStateContract.Constants
 import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
+import androidx.core.view.children
+import com.example.androidpracticumcustomview.ui.DURATION_OF_ALPHA
+import com.example.androidpracticumcustomview.ui.DURATION_OF_MOVEMENT
+import com.example.androidpracticumcustomview.ui.MAX_CHILD
 
 /*
 Задание:
@@ -24,17 +29,46 @@ class CustomContainer @JvmOverloads constructor(
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        // TODO
-        // ...
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+
+        for(child in children){
+            measureChild(child, widthMeasureSpec, heightMeasureSpec)
+        }
+
+        setMeasuredDimension(width, height)
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        // TODO
-        // ...
+        val centerHorizontal = (right - left) / 2
+        val centerVertical = (bottom - top) / 2
+
+        for (child in children) {
+            val leftEdge = centerHorizontal - (child.measuredWidth / 2)
+            val topEdge = centerVertical - (child.measuredHeight / 2)
+
+            child.layout(
+                leftEdge,
+                topEdge,
+                leftEdge + child.measuredWidth,
+                topEdge + child.measuredHeight
+            )
+
+            child.alpha = 0f
+            child.animate().alpha(1f).setDuration(DURATION_OF_MOVEMENT)
+
+            when(children.indexOf(child)){
+                0 -> child.animate().y(top.toFloat()).setDuration(DURATION_OF_MOVEMENT).start()
+                1 -> child.animate().y(bottom.toFloat() - child.measuredHeight).setDuration(DURATION_OF_MOVEMENT).start()
+            }
+        }
+
     }
 
     override fun addView(child: View) {
-        // TODO
-        // ...
+        if (childCount >= MAX_CHILD) {
+            throw IllegalStateException("Превышено количество дочерних View!")
+        }
+        super.addView(child)
     }
 }
